60af8e9534b8610ad39619f8cb594a1754cc71f7
jwnukoski <jwnukoski@protonmail.com>
2024-01-23 22:27:34 -0500

    start basic testing

--- src/structs/tree.ts
+++ src/structs/tree.ts
@@ -1,28 +1,12 @@
 export class Tree<T> {
-  readonly #root: TreeNode<T>
+  root: TreeNode<T>
 
   /**
-     *
+     * @description Creates a Tree with a root node.
      * @param value The value of the root node.
      */
   constructor (value: T) {
-    this.#root = new TreeNode(value)
-  }
-
-  /**
-     *
-     * @param value The value of the node to add.
-     * @param parentValue The value of the parent node.
-     */
-  add (value: T, parentValue) {
-    const newNode = new TreeNode(value)
-    const parent = this.findBFS(parentValue)
-
-    if (parent) {
-      parent.addChild(newNode)
-    } else {
-      throw new Error('Parent not found.')
-    }
+    this.root = new TreeNode(value)
   }
 }
 
--- tests/structs/tree.test.ts
+++ tests/structs/tree.test.ts
@@ -2,8 +2,19 @@ import { expect, test, describe } from 'bun:test'
 import { Tree, TreeNode } from '../../src/structs/tree'
 
 describe('Tree', () => {
-//   test('creates a Stack with no input array', () => {
-//     const stack = new Stack()
-//     expect(stack).toBeInstanceOf(Stack)
-//   })
+  test('creates a Tree with a root node', () => {
+    const tree = new Tree(1)
+    expect(tree).toBeInstanceOf(Tree)
+    expect(tree).toHaveProperty('root')
+    expect(tree.root).toBeInstanceOf(TreeNode)
+    expect(tree.root.value).toEqual(1)
+  })
+
+  test('can add a child node to the root node', () => {
+    const tree = new Tree(1)
+    const childNode = new TreeNode(2)
+    tree.root.addChild(childNode)
+    expect(tree.root.children).toHaveLength(1)
+    expect(tree.root.children[0].value).toEqual(2)
+  })
 })
